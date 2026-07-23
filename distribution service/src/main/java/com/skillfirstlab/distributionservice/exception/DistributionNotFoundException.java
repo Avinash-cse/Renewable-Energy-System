6ecd53bd106fe415685e6b3553923171bbb51966
Avinash-cse <avinashkarthick07@gmail.com>
2026-07-23 14:45:37 +0530

    Delete distribution service directory

--- distribution service/src/main/java/com/skillfirstlab/distributionservice/exception/DistributionNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.skillfirstlab.distributionservice.exception;
-
-public class DistributionNotFoundException extends RuntimeException {
-
-    public DistributionNotFoundException(String message) {
-        super(message);
-    }
-}
